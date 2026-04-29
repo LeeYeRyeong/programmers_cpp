5d78626db4f2121b1803d2c7d257b94e4e1389ee
ryxxng <i876838@gmail.com>
2026-04-29 22:38:26 +0900

    programmers : 두 수의 나눗셈

---
+++ programmers_LV.0/programmers120806/programmers120806.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int num1, int num2) {
+	double div = static_cast<double>(num1) / num2;
+	int answer = div * 1000;
+	return answer;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << solution(a, b);
+
+	return 0;
+}
