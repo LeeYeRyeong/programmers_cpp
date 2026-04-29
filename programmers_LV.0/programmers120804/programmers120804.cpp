5fddd5e7efe494f12564b61959d3808cb9a0b51d
ryxxng <i876838@gmail.com>
2026-04-29 22:36:24 +0900

    programmers : 두 수의 곱 구하기

---
+++ programmers_LV.0/programmers120804/programmers120804.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int solution(int num1, int num2) {
+	int answer = num1 * num2;
+	return answer;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << solution(a, b);
+
+	return 0;
+}
