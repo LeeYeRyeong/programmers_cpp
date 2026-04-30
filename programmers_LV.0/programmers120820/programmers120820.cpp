a1d773f846ea360c014922d629019a258f7bd772
ryxxng <i876838@gmail.com>
2026-04-30 23:40:58 +0900

    programmers : 나이 출력

---
+++ programmers_LV.0/programmers120820/programmers120820.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int age) {
+	int answer = 2022 - age + 1;
+	return answer;
+}
+
+int main()
+{
+	int age;
+	cin >> age;
+	cout << solution(age);
+
+	return 0;
+}
