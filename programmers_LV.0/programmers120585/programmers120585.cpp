8ed7b8202fcf579bc93d127dc159e463e1a24808
ryxxng <i876838@gmail.com>
2026-05-01 10:02:50 +0900

    programmers : 머쓱이보다 키 큰 사람

---
+++ programmers_LV.0/programmers120585/programmers120585.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> array, int height) {
+	int answer = 0;
+	for (int i = 0; i < array.size(); i++) {
+		if (array[i] > height) answer++;
+	}
+	return answer;
+}
+
+int main()
+{
+	vector<int> array(100);
+	int height, n;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> array[i];
+	cin >> height;
+	cout << solution(array, height);
+
+	return 0;
+}
