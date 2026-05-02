6e17042c715ddb11522dd9cf5c4a61422407c747
ryxxng <i876838@gmail.com>
2026-05-02 09:54:35 +0900

    programmers : 피자 나눠 먹기 (1)

---
+++ programmers_LV.0/programmers120814/programmers120814.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int n) {
+	int answer = (n % 7 == 0 ? n / 7 : n / 7 + 1);
+	return answer;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << solution(n);
+
+	return 0;
+}
