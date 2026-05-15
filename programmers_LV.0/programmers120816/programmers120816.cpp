b7e67fa0548413f6705ced18c9e52049ce699d76
ryxxng <i876838@gmail.com>
2026-05-15 23:50:02 +0900

    programmers : 피자 나눠 먹기 (3)

---
+++ programmers_LV.0/programmers120816/programmers120816.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int slice, int n) {
+	int answer = (n % slice == 0 ? n / slice : n / slice + 1);
+	return answer;
+}
+
+int main()
+{
+	int slice, n;
+	cin >> slice >> n;
+	cout << solution(slice, n);
+
+	return 0;
+}
