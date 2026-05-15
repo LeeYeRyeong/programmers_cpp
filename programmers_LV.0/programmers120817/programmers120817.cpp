e506eafc3c462218badf3b23ea0a131db03770b4
ryxxng <i876838@gmail.com>
2026-05-16 00:20:44 +0900

    programmers : 배열의 평균값

---
+++ programmers_LV.0/programmers120817/programmers120817.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+double solution(vector<int> numbers) {
+	double answer, sum = 0;
+	for (int i = 0; i < numbers.size(); i++) sum += numbers[i];
+	answer = sum / numbers.size();
+	return answer;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> numbers(n);
+	for (int i = 0; i < n; i++) cin >> numbers[i];
+	cout << fixed << setprecision(1) << solution(numbers);
+
+	return 0;
+}
