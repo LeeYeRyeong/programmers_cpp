561655f84bde26c4a777136612c7ef75cf0b3dde
ryxxng <i876838@gmail.com>
2026-05-22 09:49:04 +0900

    programmers : 아이스 아메리카노

---
+++ programmers_LV.0/programmers120819/programmers120819.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int money) {
+	vector<int> answer(2);
+	answer[0] = money / 5500;
+	answer[1] = money - (5500 * answer[0]);
+	return answer;
+}
+
+int main()
+{
+	vector<int> answer(2);
+	int money;
+	cin >> money;
+	answer = solution(money);
+	for (int i = 0; i < 2; i++) cout << answer[i] << ' ';
+
+	return 0;
+}
