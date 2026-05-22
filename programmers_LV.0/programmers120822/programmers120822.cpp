719b7fc3b7f8c9841fe44067d6d082b33cd7c596
ryxxng <i876838@gmail.com>
2026-05-22 09:49:59 +0900

    programmers : 뒤집힌 문자열

---
+++ programmers_LV.0/programmers120822/programmers120822.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string solution(string my_string) {
+	string answer = my_string;
+	for (int i = my_string.size() - 1; i >= 0; i--) answer[my_string.size() - i - 1] = my_string[i];
+	return answer;
+}
+int main()
+{
+	string my_string, answer;
+	cin >> my_string;
+	cout << solution(my_string);
+
+	return 0;
+}
