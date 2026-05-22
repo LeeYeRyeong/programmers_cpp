53ebc871e22dc5a3f399c923792a2ea5faa4edbb
ryxxng <i876838@gmail.com>
2026-05-22 09:49:32 +0900

    programmers : 배열 뒤집기

---
+++ programmers_LV.0/programmers120821/programmers120821.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+	vector<int> answer(num_list.size());
+	for (int i = 0; i < num_list.size(); i++) answer[num_list.size() - i - 1] = num_list[i];
+	return answer;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> num_list(n), answer(n);
+	for (int i = 0; i < n; i++) cin >> num_list[i];
+	answer = solution(num_list);
+	for (int i = 0; i < n; i++) cout << answer[i] << ' ';
+
+	return 0;
+}
