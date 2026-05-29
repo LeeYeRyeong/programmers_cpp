65fe90f0fe2deabd327f9f6f05a0ae165e3d97fa
ryxxng <i876838@gmail.com>
2026-05-29 23:45:49 +0900

    programmers : 문자 반복 출력하기

---
+++ programmers_LV.0/programmers120825/programmers120825.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string solution(string my_string, int n) {
+	string answer = "";
+	for (int i = 0; i < my_string.size(); i++) {
+		for (int j = 0; j < n; j++) answer += my_string[i];
+	}
+	return answer;
+}
+
+int main()
+{
+	string s;
+	int n;
+	cin >> s >> n;
+	cout << solution(s, n);
+
+	return 0;
+}
