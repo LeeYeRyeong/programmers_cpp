5e07b3755e80583f624584806a3d539e05ea6764
ryxxng <i876838@gmail.com>
2026-05-29 23:46:22 +0900

    programmers : 특정 문자 제거하기

---
+++ programmers_LV.0/programmers120826/programmers120826.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string solution(string my_string, string letter) {
+	string answer = "";
+	for (int i = 0; i < my_string.size(); i++) {
+		if (my_string[i] != letter[0]) answer += my_string[i];
+	}
+	return answer;
+}
+
+int main()
+{
+	string s, c;
+	cin >> s >> c;
+	cout << solution(s, c);
+
+	return 0;
+}
