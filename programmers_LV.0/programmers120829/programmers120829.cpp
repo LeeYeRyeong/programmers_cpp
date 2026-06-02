bc8d676d9ba24bceeab0c4fe99ebffd13333eecf
ryxxng <i876838@gmail.com>
2026-06-02 23:18:40 +0900

    programmers : 각도기

---
+++ programmers_LV.0/programmers120829/programmers120829.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int angle) {
+	if (angle < 90) return 1;
+	else if (angle == 90) return 2;
+	else if (angle < 180) return 3;
+	else if (angle == 180) return 4;
+}
+
+int main()
+{
+	int angle;
+	cin >> angle;
+	cout << solution(angle);
+
+	return 0;
+}
