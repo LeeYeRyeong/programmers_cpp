3f72e8f080cfe7bc0d61bfac225847b53cdbe82e
ryxxng <i876838@gmail.com>
2026-06-02 23:19:10 +0900

    programmers : 양꼬치

---
+++ programmers_LV.0/programmers120830/programmers120830.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int n, int k) {
+	int answer = n * 12000 + (k - (n / 10)) * 2000;
+	return answer;
+}
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	cout << solution(n, k);
+
+	return 0;
+}
