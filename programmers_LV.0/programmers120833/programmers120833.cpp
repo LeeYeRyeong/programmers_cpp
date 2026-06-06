5abf61a18162ab0e07945fb26a608820e8df794f
ryxxng <i876838@gmail.com>
2026-06-07 01:20:26 +0900

    programmers : 배열 자르기

---
+++ programmers_LV.0/programmers120833/programmers120833.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> numbers, int num1, int num2) {
+	vector<int> answer(num2 - num1 + 1);
+	for (int i = num1; i <= num2; i++) answer[i - num1] = numbers[i];
+	return answer;
+}
+
+int main()
+{
+	int n, num1, num2;
+	cin >> n;
+	vector<int> numbers(n);
+	for (int i = 0; i < n; i++) cin >> numbers[i];
+	cin >> num1 >> num2;
+	vector<int> answer(num2 - num1 + 1);
+	answer = solution(numbers, num1, num2);
+	for (int i = 0; i < answer.size(); i++) cout << answer[i] << ' ';
+
+	return 0;
+}
