3c7b47329656974b74f9e406b114307d42484e72
ryxxng <i876838@gmail.com>
2026-06-07 01:21:13 +0900

    programmers : 순서쌍의 개수

---
+++ programmers_LV.0/programmers120836/programmers120836.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) answer++;
+	}
+	return answer;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << solution(n);
+
+	return 0;
+}
