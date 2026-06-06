26779b7031a70d931baa3477b183b1f3338bc995
ryxxng <i876838@gmail.com>
2026-06-07 01:21:44 +0900

    programmers : 점의 위치 구하기

---
+++ programmers_LV.0/programmers120841/programmers120841.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> dot) {
+	if (dot[0] > 0 && dot[1] > 0) return 1;
+	else if (dot[0] < 0 && dot[1] > 0) return 2;
+	else if (dot[0] < 0 && dot[1] < 0) return 3;
+	else if (dot[0] > 0 && dot[1] < 0) return 4;
+}
+
+int main()
+{
+	vector<int> dot(2);
+	cin >> dot[0] >> dot[1];
+	cout << solution(dot);
+
+	return 0;
+}
