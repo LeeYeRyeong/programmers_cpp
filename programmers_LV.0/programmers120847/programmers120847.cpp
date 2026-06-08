b40a802d980337412059b02df5cc9e763aadfaa9
ryxxng <i876838@gmail.com>
2026-06-08 23:47:32 +0900

    programmers : 최댓값 만들기(1)

---
+++ programmers_LV.0/programmers120847/programmers120847.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> numbers) {
+	int answer = 1, max = 0, maxIndex = -1;
+	for (int i = 0; i < numbers.size(); i++) {
+		if (numbers[i] > max) {
+			max = numbers[i];
+			maxIndex = i;
+		}
+	}
+	answer *= max;
+	max = 0; 
+	for (int i = 0; i < numbers.size(); i++) {
+		if (numbers[i] > max && maxIndex != i) max = numbers[i];
+	}
+	answer *= max;
+	return answer;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector <int> numbers(n);
+	for (int i = 0; i < n; i++) cin >> numbers[i];
+	cout << solution(numbers);
+
+	return 0;
+}
