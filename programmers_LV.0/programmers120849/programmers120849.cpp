2c541cc2da54f0041ef9eaf8d45a4eda95087d87
ryxxng <i876838@gmail.com>
2026-06-09 01:18:27 +0900

    programmers : 모음 제거

---
+++ programmers_LV.0/programmers120849/programmers120849.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string solution(string my_string) {
+	string answer = "";
+	for (int i = 0; i < my_string.size(); i++) {
+		if (!(my_string[i] == 'a' || my_string[i] == 'e' || my_string[i] == 'i' || my_string[i] == 'o' || my_string[i] == 'u')) answer += my_string[i];
+	}
+	return answer;
+}
+
+int main()
+{
+	string s;
+	getline(cin, s);
+	cout << solution(s);
+
+	return 0;
+}
