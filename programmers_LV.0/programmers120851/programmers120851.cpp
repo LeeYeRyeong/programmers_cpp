a008d74ab130c7f2b032339007a90ff45470ef15
ryxxng <i876838@gmail.com>
2026-06-09 01:19:04 +0900

    programmers : 숨어있는 숫자의 덧셈 (1)

---
+++ programmers_LV.0/programmers120851/programmers120851.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int solution(string my_string) {
+	int answer = 0;
+	for (int i = 0; i < my_string.size(); i++) {
+		if (my_string[i] >= '0' && my_string[i] <= '9') answer += my_string[i] - '0';
+	}
+	return answer;
+}
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << solution(s);
+
+	return 0;
+}
