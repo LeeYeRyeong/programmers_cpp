1362d1faec772ea156f51c410276c94b53f38f6e
ryxxng <i876838@gmail.com>
2026-06-19 00:05:43 +0900

    programmers : 대문자와 소문자

---
+++ programmers_LV.0/programmers120893/programmers120893.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string solution(string my_string) {
+	string answer = "";
+	for (int i = 0; i < my_string.size(); i++) {
+		if (my_string[i] >= 'a' && my_string[i] <= 'z') answer.push_back(my_string[i] - 32);
+		else if (my_string[i] >= 'A' && my_string[i] <= 'Z') answer.push_back(my_string[i] + 32);
+	}
+	return answer;
+}
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << solution(s);
+
+	return 0;
+}
