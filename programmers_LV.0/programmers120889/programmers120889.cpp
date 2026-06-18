2082f514baefdf86da3aeebbfec7bff5ef21de97
ryxxng <i876838@gmail.com>
2026-06-19 00:05:02 +0900

    programmers : 삼각형 완성조건(1)\

---
+++ programmers_LV.0/programmers120889/programmers120889.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> sides) {
+	int answer, max = 0, sum = 0;
+	for (int i = 0; i < 3; i++) {
+		if (max < sides[i]) {
+			sum += max;
+			max = sides[i];
+		}
+		else sum += sides[i];
+	}
+	answer = (max < sum ? 1 : 2);
+	return answer;
+}
+int main()
+{
+	vector<int> sides(3);
+	for (int i = 0; i < 3; i++) cin >> sides[i];
+	cout << solution(sides);
+
+	return 0;
+}
