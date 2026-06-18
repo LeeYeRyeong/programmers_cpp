c27843cd980650d5c85e96cb355281d7e49d6050
ryxxng <i876838@gmail.com>
2026-06-18 23:42:02 +0900

    programmers : 배열 원소의 길이

---
+++ programmers_LV.0/programmers120854/programmers120854.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+	vector<int> answer(strlist.size());
+	for (int i = 0; i < strlist.size(); i++) {
+		int count = 0;
+		for (int j = 0; j < strlist[i].size(); j++) count++;
+		answer[i] = count;
+	}
+	return answer;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<string> strlist(n);
+	vector<int> answer(n);
+	for (int i = 0; i < n; i++) cin >> strlist[i];
+	answer = solution(strlist);
+	for (int i = 0; i < n; i++) cout << answer[i] << ' ';
+
+	return 0;
+}
