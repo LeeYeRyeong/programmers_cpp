9a0aa00a6e33579db2a2a0e061c3c46330442f0e
ryxxng <i876838@gmail.com>
2026-06-20 00:44:26 +0900

    programmers : 편지

---
+++ programmers_LV.0/programmers120898/programmers120898.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int solution(string message) {
+	int answer = message.size() * 2;
+	return answer;
+}
+
+int main()
+{
+	string message;
+	getline(cin, message);
+	cout << solution(message);
+
+	return 0;
+}
