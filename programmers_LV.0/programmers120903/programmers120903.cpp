4352106d40b9c9b325e8011f483e2b19d3726da6
ryxxng <i876838@gmail.com>
2026-06-20 00:45:46 +0900

    programmers : 배열의 유사도

---
+++ programmers_LV.0/programmers120903/programmers120903.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int solution(vector<string>s1, vector<string> s2) {
+	int answer = 0;
+	for (int i = 0; i < s1.size(); i++) {
+		for (int j = 0; j < s2.size(); j++) {
+			if (s1[i] == s2[j]) answer++;
+		}
+	}
+	return answer;
+}
+
+int main()
+{
+	int n1, n2;
+	cin >> n1 >> n2;
+	vector<string> s1(n1), s2(n2);
+	for (int i = 0; i < n1; i++) cin >> s1[i];
+	for (int i = 0; i < n2; i++) cin >> s2[i];
+	cout << solution(s1, s2);
+
+	return 0;
+}
