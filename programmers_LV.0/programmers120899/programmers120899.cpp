32f058c523ded717b697a7f86936bf8868d7e36d
ryxxng <i876838@gmail.com>
2026-06-20 00:45:05 +0900

    programmers : 가장 큰 수 찾기

---
+++ programmers_LV.0/programmers120899/programmers120899.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> array) {
+	vector<int> answer(2);
+	answer[0] = -1, answer[1] = -1;
+	for (int i = 0; i < array.size(); i++) {
+		if (answer[0] < array[i]) {
+			answer[0] = array[i];
+			answer[1] = i;
+		}
+	}
+	return answer;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> array(n), answer(2);
+	for (int i = 0; i < n; i++) cin >> array[i];
+	answer = solution(array);
+	for (int i = 0; i < n; i++) cout << answer[i] << ' ';
+
+	return 0;
+}
