f3915a5fa95e7b4fa3cb2f7146934ff06b689c18
ryxxng <i876838@gmail.com>
2026-06-25 22:45:42 +0900

    programmers : n의 배수 고르기

---
+++ programmers_LV.0/programmers120905/programmers120905.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int n, vector<int> numlist) {
+	vector<int> answer;
+	for (int i = 0; i < numlist.size(); i++) {
+		if (numlist[i] % n == 0) answer.push_back(numlist[i]);
+	}
+	return answer;
+}
+
+int main()
+{
+	int n, count;
+	cin >> n >> count;
+	vector<int> numlist(count), answer;
+	for (int i = 0; i < count; i++) cin >> numlist[i];
+	answer = solution(n, numlist);
+	for (int i = 0; i < answer.size(); i++) cout << answer[i] << ' ';
+
+	return 0;
+}
